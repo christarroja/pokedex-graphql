7c3a4bd5bb76d32b1729280c422154217512ba9b
Chris Tarroja <christopher.tarroja@getakalabs.com>
2023-10-19 11:19:41 +0800

    add headlessui and heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 // import PokemonList from '@/components/pokemonList';
 import type { Metadata } from 'next'
+import Image from 'next/image';
 
 const Home: React.FC = () => {
   return (
     <div>
       <main>
+        <Image 
+          src={'/pokemonLogo.svg'} 
+          alt={'Pokémon Logo'} 
+          width={998}
+          height={371}
+        />
         <h1>Pokemon List</h1>
         {/* <PokemonList /> */}
       </main>
