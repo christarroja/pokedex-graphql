3d2cfb33891866cfb7ebec7024677631c9567f9d
Chris Tarroja <christopher.tarroja@getakalabs.com>
2023-10-22 00:46:53 +0800

    push to vercel

--- components/pages/PokeStatsModal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PokeStatsModal = () => {
-  return (
-    <div>PokeStatsModal</div>
-  )
-}
-
-export default PokeStatsModal
--- lib/apolloProvider.tsx
+++ lib/apolloProvider.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import { ApolloLink, HttpLink } from "@apollo/client";
 import {
